040c4228bd23ce2570c5a4ba5939089af49d2600
JulesDelorme <jules.delorme51@gmail.com>
2026-02-25 11:54:09 +0100

    Initial Commit

---
+++ FlirtifyTests/FlirtifyTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlirtifyTests.swift
+//  FlirtifyTests
+//
+//  Created by Jules Delorme on 25/02/2026.
+//
+
+import Testing
+
+struct FlirtifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
